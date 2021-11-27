c0e36bbcf0686d1acf857b1b2f03a555d93e97ca
sepfy <sepfy95@gmail.com>
2021-11-27 06:52:14 +0000

    Add missing library

--- scripts/create-dpkg.sh
+++ scripts/create-dpkg.sh
@@ -9,6 +9,7 @@ mkdir -p $BIN_PATH $LIB_PATH
 cp third_party/pear/third_party/libnice/builddir/stun/tools/stund $BIN_PATH/
 cp cmake/src/berry-share $BIN_PATH/
 cp third_party/pear/cmake/src/libpear.so $LIB_PATH
+cp third_party/pear/third_party/libnice/builddir/nice/libnice.so* $LIB_PATH
 
 sed -i "/Version/c\Version: $VERSION" dpkg/DEBIAN/control
 
